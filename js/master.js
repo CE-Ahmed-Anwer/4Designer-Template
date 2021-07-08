d551d6a2fc9cf0f82b7c7ff9ef6d0e41f414d044
Ahmed Anwer <ahmedanwer222@gmail.com>
2021-07-08 05:47:59 +0200

    Add our skills section with animation

--- js/master.js
+++ js/master.js
@@ -111,3 +111,22 @@ function randomizeImages(){
     }
 }
 randomizeImages();
+
+// Select Skills Selector
+let ourSkills = document.querySelector(".skills");
+window.onscroll = function() {
+    // Skills Offset Top
+    let skillsOffsetTop = ourSkills.offsetTop;
+    // Skills Outer Height
+    let skillsOuterHeight = ourSkills.offsetHeight;
+    // Window Height
+    let windowHeight = this.innerHeight;
+    // Window Scroll Top
+    let windowScrollTop = this.pageYOffset;
+    if (windowScrollTop > (skillsOffsetTop + skillsOuterHeight - windowHeight)) {
+        let allSkills = document.querySelectorAll(".skill-box .skill-progress span");
+        allSkills.forEach(skill => {
+            skill.style.width = skill.dataset.progress;
+        });
+    }
+};
