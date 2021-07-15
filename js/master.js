3f518e912e4305f5a4063be05d8e2d65e0fb9089
Ahmed Anwer <ahmedanwer222@gmail.com>
2021-07-15 04:50:33 +0200

    Add menu for small screen

--- js/master.js
+++ js/master.js
@@ -241,3 +241,31 @@ document.querySelector(".reset-options").onclick = function(){
     // Reload Window
     window.location.reload();
 };
+
+// Toggle Menu
+let toggleButton = document.querySelector(".toggle-menu");
+let tLinks = document.querySelector(".links");
+toggleButton.onclick = function(e){
+    // Stop Propagation
+    e.stopPropagation();
+    // Toggle Class "menu-active" On Button
+    this.classList.toggle("menu-active");
+    // Toggle Class "open" On Links
+    tLinks.classList.toggle("open");
+};
+// Click Anywhere Outside Menu And Toggle Button
+document.addEventListener("click", (e) => {
+    if (e.target !== toggleButton && e.target !== tLinks) {
+        // Check If Menu Is Open
+        if(tLinks.classList.contains("open")){
+            // Toggle Class "menu-active" On Button
+            toggleButton.classList.toggle("menu-active");
+            // Toggle Class "open" On Links
+            tLinks.classList.toggle("open");
+        }
+    }
+});
+// Stop Propagation On Menu
+tLinks.onclick = function(e){
+    e.stopPropagation();
+};
